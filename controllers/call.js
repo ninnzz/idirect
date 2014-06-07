c371dc99b0a533104d94483178007d3d6a8a2d61
pprmint <nreclarin@gmail.com>
2014-06-07 10:41:27 +0800

    fx

--- controllers/call.js
+++ controllers/call.js
@@ -54,7 +54,7 @@ exports.call_redirect = function(req,res,next) {
                     }).toArray(function (err,results) {
                         if(err) return res.send(500,err);
                         if(results.length > 0) {
-                            tropo.transfer('+63'+results[0].data[0].contact_number[0], false, null, null, {'x-caller-name' : 'Mark Headd'}, null, null, true, '#', 60.0);
+                            tropo.transfer('sip:'+results[0].data[0].contact_number[0]+'@sip.tropo.net', false, null, null, {'x-caller-name' : 'Mark Headd'}, null, null, true, '#', 60.0);
                             res.writeHead(200, {'Content-Type': 'application/json'}); 
                             console.log(tropowebapi.TropoJSON(tropo));
                             res.end(tropowebapi.TropoJSON(tropo));
