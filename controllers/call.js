d028021a8c4b0b8b00efcaca2cb37ab2f81aa800
pprmint <nreclarin@gmail.com>
2014-06-07 10:57:29 +0800

    fx

--- controllers/call.js
+++ controllers/call.js
@@ -57,7 +57,7 @@ exports.call_redirect = function(req,res,next) {
                             var say = new Say("http://www.phono.com/audio/holdmusic.mp3");
                             var on = {"event":"ring", "say": say};
                             
-                            tropo.transfer('sip:63'+results[0].data[0].contact_number[0]+'@sip.tropo.net', null, null, null, null, on, null, null);
+                            tropo.transfer('+63'+results[0].data[0].contact_number[0], null, null, null, null, on, null, null);
                             res.writeHead(200, {'Content-Type': 'application/json'}); 
                             console.log(tropowebapi.TropoJSON(tropo));
                             res.end(tropowebapi.TropoJSON(tropo));
